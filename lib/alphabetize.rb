fdfe752c0b3e19b079645269a66b4204d21c9578
Jason Pitts <juniorjr083@gmail.com>
2018-06-17 00:36:29 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,7 +2,10 @@ def alphabetize(arr)
   # code here
   ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   
-  arr.sort_by do |prev,curr| 
+  arr.sort_by do |word|
+    word.split("").map do |c|
+      ESPERANTO_ALPHABET.index(c)
+    end 
     
     
   end 
